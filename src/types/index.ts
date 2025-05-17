9d97b03b724033bc7288d982eeccab33398c7f02
lyanbrito <lyanbrito613@gmail.com>
2025-05-16 23:30:54 -0300

    feat: created small components props

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,21 @@ export type PageProps = {
     page: string;
 };
 export type ButtonProps = {
-    title: string;
+    type: string;
     content: string;
 };
+export type CardProps = {
+    title: string;
+    content: string;
+}
 export type TitleProps = {
     index: string;
     title: string;
+}
+export type ProjectProps = {
+    projectTitle: string;
+    projectDescription: string;
+    projectAlt: string;
+    projectImg: string;
+    page: string;
 }
