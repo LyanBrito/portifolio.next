50a245d149c713d8f284431844342c0e829e5757
lyanbrito <lyanbrito613@gmail.com>
2025-05-17 11:33:42 -0300

    assets: biome format

---
+++ src/assets/icons/MenuIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgHambMenuIcon = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M3 15.5V13.5H21V15.5H3ZM3 10.5V8.5H21V10.5H3ZM3 5.5V3.5H21V5.5H3Z"
+			fill="white"
+		/>
+		<path d="M3 18.5V20.5H21V18.5H3Z" fill="white" />
+	</svg>
+);
+
+export default SvgHambMenuIcon;
---
+++ src/assets/icons/XMenuIcon.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const SvgMenuXIcon = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M5 5L19 19M19 5L5 19"
+			stroke="white"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export default SvgMenuXIcon;
