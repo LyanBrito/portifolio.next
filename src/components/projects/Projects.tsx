b55b522926963c3152086df7322b6c91240ec2af
lyanbrito <lyanbrito613@gmail.com>
2025-05-16 23:30:30 -0300

    feat: created projects component

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,40 @@
+import { TitleWrapper } from "../small-componnets/TitleWrapper";
+import "./projects.scss";
+import Project from "@/components/small-componnets/Project";
+import {PageProps} from "@/types";
+
+export function Projects({ page }: PageProps) {
+	return (
+		<div className="project-container">
+			<div className="project-title">
+				<TitleWrapper page={page} title="projects" index="/" />
+				<a href="https://example.com" target="_blank" rel="noopener noreferrer">
+					View more ~~{">"}
+				</a>
+			</div>
+			<div className="projects">
+				<Project
+					page={page}
+					projectDescription="Personal Portifolio"
+					projectTitle="Portifolio"
+					projectImg="https://picsum.photos/305/208"
+					projectAlt="portifolio image"
+				/>
+				<Project
+					page={page}
+					projectDescription="Personal Portifolio"
+					projectTitle="Portifolio"
+					projectImg="https://picsum.photos/305/208"
+					projectAlt="portifolio image"
+				/>
+				<Project
+					page={page}
+					projectDescription="Personal Portifolio"
+					projectTitle="Portifolio"
+					projectImg="https://picsum.photos/305/208"
+					projectAlt="portifolio image"
+				/>
+			</div>
+		</div>
+	);
+}
