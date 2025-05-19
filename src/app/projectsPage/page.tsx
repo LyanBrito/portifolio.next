d339251122468b1c663aa5d511407750cceacba0
lyanbrito <lyanbrito613@gmail.com>
2025-05-19 11:01:26 -0300

    wip: creating projects page

---
+++ src/app/projectsPage/page.tsx
@@ -0,0 +1,62 @@
+import Header from "@/components/Main/header/header";
+import "../page.scss";
+import "@/components/projects/projects.scss"
+import {TitleWrapper} from "@/components/small-componnets/TitleWrapper";
+import Project from "@/components/small-componnets/Project";
+import Footer from "@/components/footer/Footer";
+
+export const metadata = {
+    title: "Projects | Lyan Brito",
+};
+
+export default function Page({}) {
+    return (
+        <div className="page">
+            <Header index="\projects&gt;" page="pg2"/>
+            <div className="all-projects">
+                <TitleWrapper page="pg2" index="/" title="Projects"/>
+                <div className="projects">
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                    />{" "}
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                    />{" "}
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                    />
+                </div>
+                <div className="small-projects">
+
+                </div>
+            </div>
+            <Footer page="pg2"/>
+        </div>
+    );
+}
