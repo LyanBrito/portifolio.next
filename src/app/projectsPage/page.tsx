40a4d4a8cd5271655e0a6fb50d5818971a098c3d
lyanbrito <lyanbrito613@gmail.com>
2025-05-19 21:07:18 -0300

    feat: created projects page

--- src/app/projectsPage/page.tsx
+++ src/app/projectsPage/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Main/header/header";
 import "../page.scss";
 import "@/components/projects/projects.scss"
-import {TitleWrapper} from "@/components/small-componnets/TitleWrapper";
+import { TitleWrapper } from "@/components/small-componnets/TitleWrapper";
 import Project from "@/components/small-componnets/Project";
 import Footer from "@/components/footer/Footer";
 
@@ -9,12 +9,12 @@ export const metadata = {
     title: "Projects | Lyan Brito",
 };
 
-export default function Page({}) {
+export default function Page({ }) {
     return (
         <div className="page">
-            <Header index="\projects&gt;" page="pg2"/>
+            <Header index="\projects&gt;" page="pg2" />
             <div className="all-projects">
-                <TitleWrapper page="pg2" index="/" title="Projects"/>
+                <TitleWrapper page="pg2" index="/" title="projects" />
                 <div className="projects">
                     <Project
                         page="pg2"
@@ -22,6 +22,7 @@ export default function Page({}) {
                         projectTitle="Portifolio"
                         projectImg="https://picsum.photos/305/208"
                         projectAlt="portifolio image"
+                        type="pj1"
                     />
                     <Project
                         page="pg2"
@@ -29,6 +30,7 @@ export default function Page({}) {
                         projectTitle="Portifolio"
                         projectImg="https://picsum.photos/305/208"
                         projectAlt="portifolio image"
+                        type="pj1"
                     />
                     <Project
                         page="pg2"
@@ -36,6 +38,7 @@ export default function Page({}) {
                         projectTitle="Portifolio"
                         projectImg="https://picsum.photos/305/208"
                         projectAlt="portifolio image"
+                        type="pj1"
                     />{" "}
                     <Project
                         page="pg2"
@@ -43,6 +46,7 @@ export default function Page({}) {
                         projectTitle="Portifolio"
                         projectImg="https://picsum.photos/305/208"
                         projectAlt="portifolio image"
+                        type="pj1"
                     />{" "}
                     <Project
                         page="pg2"
@@ -50,13 +54,64 @@ export default function Page({}) {
                         projectTitle="Portifolio"
                         projectImg="https://picsum.photos/305/208"
                         projectAlt="portifolio image"
+                        type="pj1"
                     />
-                </div>
-                <div className="small-projects">
-
                 </div>
             </div>
-            <Footer page="pg2"/>
+            <div className="small-projects">
+                <TitleWrapper page="pg2" index="/" title="small-projects" />
+                <div className="projects">
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                    <Project
+                        page="pg2"
+                        projectDescription="Personal Portifolio"
+                        projectTitle="Portifolio"
+                        projectImg="https://picsum.photos/305/208"
+                        projectAlt="portifolio image"
+                        type="pj2"
+                    />
+                </div>
+            </div>
+            <Footer page="pg2" />
         </div>
     );
 }
