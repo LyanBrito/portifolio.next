1cca13a549262f80d97f35e4dd12099b435e3673
lyanbrito <lyanbrito613@gmail.com>
2025-05-20 10:49:30 -0300

    assets: added new images

--- src/assets/images/export.ts
+++ src/assets/images/export.ts
@@ -1,1 +1,6 @@
+export { default as LandwindImg } from "./landwind-image.png";
+export { default as RestfulImg } from "./restful-image.png";
+export { default as RRImg } from "./rr-image.png";
 export { default as PlaceholderImage } from "./395.webp";
+export { default as FashionImg } from "./fashion-img.png";
+export { default as JavaImg } from "./java-img.png";
