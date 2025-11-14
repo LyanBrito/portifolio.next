3da43b16f06e5721e3a45af7c53b7bc2ee0a76be
lyanbrito <lyanbrito613@gmail.com>
2025-11-13 22:09:24 -0300

    wip: creating sidebar component

--- apps/portifolio-content/src/components/NavBar/NavBar.tsx
+++ apps/portifolio-content/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,28 @@
 import styles from "./Navbar.module.scss";
+import Link from "next/link";
+import MoreOptions from "@/assets/icons/MoreOptions";
+import {useState} from "react";
 
-export default function NavBar({ page }: any) {
+export default function NavBar({page}: any) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuClose = () => {
+        setMenuOpen(false)
+    }
+    const handleMenuToggle = () => setMenuOpen(!menuOpen);
+
     return (
         <nav className={styles.navContainer}>
-            <a className={page} href="">Home</a>
-            <a className={page} href="">Projects</a>
-            <a className={page} href="">About Me</a>
-            <a className={page} href="">Contacts</a>
+            <div className={`${styles.menuContent} ${menuOpen ? styles.opened : ""}`}>
+                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Home</Link>
+                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Projects</Link>
+                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">About Me</Link>
+                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Contacts</Link>
+            </div>
+            <button className={styles.menu} onClick={handleMenuToggle}>
+                <MoreOptions />
+            </button>
         </nav>
+
     )
 }
