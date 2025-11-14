7836a6fa552619330799af621ed41d47a8c9ad88
lyanbrito <lyanbrito613@gmail.com>
2025-11-14 11:16:03 -0300

    feat: created NabBar component

--- apps/portifolio-content/src/components/NavBar/NavBar.tsx
+++ apps/portifolio-content/src/components/NavBar/NavBar.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 import MoreOptions from "@/assets/icons/MoreOptions";
 import {useState} from "react";
 
-export default function NavBar({page}: any) {
+type NavbarProps = {
+    page: string;
+};
+
+export default function NavBar({page}: NavbarProps) {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const handleMenuClose = () => {
@@ -14,10 +18,10 @@ export default function NavBar({page}: any) {
     return (
         <nav className={styles.navContainer}>
             <div className={`${styles.menuContent} ${menuOpen ? styles.opened : ""}`}>
-                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Home</Link>
-                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Projects</Link>
-                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">About Me</Link>
-                <Link className={`${styles[page]} ${styles.link}`} onClick={handleMenuClose} href="">Contacts</Link>
+                <Link className={`${styles.link} ${page == "home" ? styles.selected : ""}`} onClick={handleMenuClose} href="">Home</Link>
+                <Link className={`${styles.link} ${page == "projects" ? styles.selected : ""}`} onClick={handleMenuClose} href="">Projects</Link>
+                <Link className={`${styles.link} ${page == "about-me" ? styles.selected : ""}`} onClick={handleMenuClose} href="">About Me</Link>
+                <Link className={`${styles.link} ${page == "contacts" ? styles.selected : ""}`} onClick={handleMenuClose} href="">Contacts</Link>
             </div>
             <button className={styles.menu} onClick={handleMenuToggle}>
                 <MoreOptions />
