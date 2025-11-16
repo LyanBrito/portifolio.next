65f1c6aa8b31b578b557b2bfb00aad6720b90d36
lyanbrito <lyanbrito613@gmail.com>
2025-11-16 20:35:48 -0300

    feat: styled info % about me sction on HomePage.tsx

--- apps/portifolio-content/src/pages/HomePage/HomePage.tsx
+++ apps/portifolio-content/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,42 @@
 import NavBar from "@/components/NavBar/NavBar"
 import s from "./HomePage.module.scss"
+import StatusActive from "@/assets/icons/StatusActive";
+import Link from "next/link";
+import SkillsCard from "@/components/SkillsCard/Skills";
+import ReactIcon from "@/assets/icons/ReactIcon";
+import TSIcon from "@/assets/icons/TSIcon";
+import SpringIcon from "@/assets/icons/SpringIcon";
+import FigmaIcon from "@/assets/icons/FigmaIcon";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
 
 export default function HomePage() {
     return (
         <div className={s.homeContainer}>
-            <NavBar page="home" />
-            <section className={s.titleWrapper}>
-                <h1>Lyan Brito Web Developer</h1>
-                <p>Hi! I'm a developer passionate in UI/UX & Web Development. Throughout my journey, I gained experience in several areas. Take a look on my works.</p>
+            <NavBar page="home"/>
+            <section className={s.textWrapper}>
+                <div className={s.titleWrapper}>
+                    <h1>Lyan Brito</h1>
+                    <h1>Web Developer</h1>
+                </div>
                 <div className={s.ATWcontainer}>
-                    {/* svg da bolinha verde, manipulo a animação depois */}
-                    <p>Available To Work</p>
+                    <StatusActive/>
+                    <span>Available To Work</span>
+                </div>
+                <p>Hi! I'm a developer passionate in UI/UX & Web Development. Throughout my journey, I gained experience
+                    in several areas. Take a look on my works.</p>
+            </section>
+            <section className={s.aboutMeContainer}>
+                <div className={s.aboutMeWrapper}>
+                    <div className={s.aboutMeTitle}>
+                        <h2>About me</h2>
+                        <p>Here some special skills that i learned in my trajectory</p>
+                    </div>
+                    <Link href="#">More <ArrowIcon /></Link>
+                </div>
+                <div className={s.cardWrapper}>
+                    <SkillsCard type={"react"} title={"React"} icon={<ReactIcon />}/>
+                    <SkillsCard type={"figma"} title={"Design UI/UX"} icon={<FigmaIcon />}/>
+                    <SkillsCard type={"spring"} title={"Spring Boot"} icon={<SpringIcon />}/>
                 </div>
             </section>
         </div>
