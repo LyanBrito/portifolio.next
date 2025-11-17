16869be8c133fda43c82e23876e8d584c34fab0d
lyanbrito <lyanbrito613@gmail.com>
2025-11-16 23:00:47 -0300

    chore: minor changes

--- apps/portifolio-content/src/components/SkillsCard/Skills.tsx
+++ apps/portifolio-content/src/components/SkillsCard/Skills.tsx
@@ -1,17 +1,21 @@
 import s from './Skills.module.scss'
 import {ReactElement} from "react";
+import Link from "next/link";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
 
 type CardProps = {
     type: keyof typeof s,
     title: string,
     icon: any,
+    link: string,
 }
 
-export default function SkillsCard({type, title, icon}: CardProps) {
+export default function SkillsCard({type, title, icon, link}: CardProps) {
     return (
         <div className={`${s[type]} ${s.cardContainer}`}>
             {icon}
             <p>{title}</p>
+            <Link href={link}>See documentation <ArrowIcon /></Link>
         </div>
     )
 }
