46f0a9f7548d3dd0315702547e0cdbb478bdff95
lyanbrito <lyanbrito613@gmail.com>
2025-11-17 16:46:31 -0300

    feat: update skills icons to use React components

--- apps/portifolio-content/src/components/Projecs/skillsMap.ts
+++ apps/portifolio-content/src/components/Projecs/skillsMap.ts
@@ -1,3 +1,25 @@
-export const skillsIcons: Record<string, string> = {
-    "React": "../assets/icons/ReactIcon",
+import SpringIcon from "@/assets/icons/SpringIcon";
+import ReactIcon from "../../assets/icons/ReactIcon"
+import GitIcon from "@/assets/icons/GitIcon";
+import FigmaIcon from "@/assets/icons/FigmaIcon";
+import TSIcon from "@/assets/icons/TSIcon";
+import JSIcon from "@/assets/icons/JSIcon";
+import HTMLIcon from "@/assets/icons/HTMLIcon";
+import CSSIcon from "@/assets/icons/CSSIcon";
+import SCSSIcon from "@/assets/icons/SCSSIcon";
+import NextIcon from "@/assets/icons/NextIcon";
+import JavaIcon from "@/assets/icons/JavaIcon";
+
+export const skillsIcons: Record<string, React.FC> = {
+  React: ReactIcon,
+  Spring: SpringIcon,
+  Git: GitIcon,
+  Figma: FigmaIcon,
+  TypeScript: TSIcon,
+  JavaScript: JSIcon,
+  HTML: HTMLIcon,
+  CSS: CSSIcon,
+  SCSS: SCSSIcon,
+  NextJS: NextIcon,
+  Java: JavaIcon,
 };
