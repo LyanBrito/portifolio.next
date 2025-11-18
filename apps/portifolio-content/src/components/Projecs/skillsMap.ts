5e51eb7fcc81257a2871a30fc81c5893bd83ea9e
lyanbrito <lyanbrito613@gmail.com>
2025-11-17 21:16:51 -0300

    feat: installed devicons-react

--- apps/portifolio-content/src/components/Projecs/skillsMap.ts
+++ apps/portifolio-content/src/components/Projecs/skillsMap.ts
@@ -1,25 +1,29 @@
-import SpringIcon from "@/assets/icons/SpringIcon";
-import ReactIcon from "../../assets/icons/ReactIcon"
-import GitIcon from "@/assets/icons/GitIcon";
-import FigmaIcon from "@/assets/icons/FigmaIcon";
-import TSIcon from "@/assets/icons/TSIcon";
-import JSIcon from "@/assets/icons/JSIcon";
-import HTMLIcon from "@/assets/icons/HTMLIcon";
-import CSSIcon from "@/assets/icons/CSSIcon";
-import SCSSIcon from "@/assets/icons/SCSSIcon";
-import NextIcon from "@/assets/icons/NextIcon";
-import JavaIcon from "@/assets/icons/JavaIcon";
+import React from "react";
+
+import {
+    AzuresqldatabaseOriginal,
+    Css3Original,
+    FigmaOriginal,
+    GitOriginal, Html5Original,
+    JavaOriginal, JavascriptOriginal, MysqlOriginal, NextjsOriginal,
+    ReactOriginal, SassOriginal,
+    SpringOriginal, SqlalchemyOriginal, SqldeveloperOriginal, StyledcomponentsOriginal, TailwindcssOriginal,
+    TypescriptOriginal
+} from "devicons-react";
 
 export const skillsIcons: Record<string, React.FC> = {
-  React: ReactIcon,
-  Spring: SpringIcon,
-  Git: GitIcon,
-  Figma: FigmaIcon,
-  TypeScript: TSIcon,
-  JavaScript: JSIcon,
-  HTML: HTMLIcon,
-  CSS: CSSIcon,
-  SCSS: SCSSIcon,
-  NextJS: NextIcon,
-  Java: JavaIcon,
+    React: ReactOriginal,
+    Spring: SpringOriginal,
+    Git: GitOriginal,
+    Figma: FigmaOriginal,
+    TypeScript: TypescriptOriginal,
+    JavaScript: JavascriptOriginal,
+    HTML: Html5Original,
+    CSS: Css3Original,
+    SCSS: SassOriginal,
+    NextJS: NextjsOriginal,
+    Java: JavaOriginal,
+    MySQL: MysqlOriginal,
+    TailwindCSS: TailwindcssOriginal,
+    "styled-components" : StyledcomponentsOriginal,
 };
--- apps/portifolio-content/src/pages/HomePage/HomePage.tsx
+++ apps/portifolio-content/src/pages/HomePage/HomePage.tsx
@@ -3,24 +3,21 @@ import s from "./HomePage.module.scss"
 import StatusActive from "@/assets/icons/StatusActive";
 import Link from "next/link";
 import SkillsCard from "@/components/SkillsCard/Skills";
-import ReactIcon from "@/assets/icons/ReactIcon";
-import TSIcon from "@/assets/icons/TSIcon";
-import SpringIcon from "@/assets/icons/SpringIcon";
-import FigmaIcon from "@/assets/icons/FigmaIcon";
 import ArrowIcon from "@/assets/icons/ArrowIcon";
 import ProjectsCard from "@/components/Projecs/ProjectsCard";
+import {FigmaOriginal, GithubOriginal, GitOriginal, ReactOriginal} from "devicons-react";
 
 export default function HomePage() {
     return (
         <div className={s.homeContainer}>
-            <NavBar page="home" />
+            <NavBar page="home"/>
             <section className={s.textWrapper}>
                 <div className={s.titleWrapper}>
                     <h1>Lyan Brito</h1>
                     <h1>Web Developer</h1>
                 </div>
                 <div className={s.ATWcontainer}>
-                    <StatusActive />
+                    <StatusActive/>
                     <span>Available To Work</span>
                 </div>
                 <p>Hi! I'm a developer passionate in UI/UX & Web Development. Throughout my journey, I gained experience
@@ -32,18 +29,18 @@ export default function HomePage() {
                         <h3>About me</h3>
                         <p>Here some special skills that i learned in my trajectory</p>
                     </div>
-                    <Link href="#">More <ArrowIcon /></Link>
+                    <Link href="#">More <ArrowIcon/></Link>
                 </div>
                 <div className={s.cardWrapper}>
                     <SkillsCard
                         link="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://react.dev/&ved=2ahUKEwicsbe4g_iQAxWEqpUCHbi9JngQFnoECBYQAQ&usg=AOvVaw1tEjYYiD7LQlxO53dgjTHV"
-                        type="react" title="React" icon={<ReactIcon />} />
+                        type="react" title="React" icon={<ReactOriginal/>}/>
                     <SkillsCard
-                        link="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://translate.google.com/translate%3Fu%3Dhttps://www.figma.com/%26hl%3Dpt%26sl%3Den%26tl%3Dpt%26client%3Dwa&ved=2ahUKEwiQ5ZfKg_iQAxVHpJUCHY3qI08QFnoECCYQAw&usg=AOvVaw0-Ta0uXMAObrHG2ij5pSgr"
-                        type="figma" title="Design UI/UX" icon={<FigmaIcon />} />
+                        link="https://www.figma.com/pt-br/design/"
+                        type="figma" title="Design UI/UX" icon={<FigmaOriginal/>}/>
                     <SkillsCard
-                        link="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://spring.io/projects/spring-boot/&ved=2ahUKEwiS4d_Zg_iQAxVYqpUCHb8DNygQFnoECA4QAQ&usg=AOvVaw2pUMdt8wsd8iwcI6VR-oxY"
-                        type="spring" title="Spring Boot" icon={<SpringIcon />} />
+                        link="https://git-scm.com/book/ms/v2/Getting-Started-About-Version-Control"
+                        type="git" title="Git Versioning" icon={<GitOriginal/>}/>
                 </div>
             </section>
             <section className={s.sectionContainer}>
@@ -52,11 +49,12 @@ export default function HomePage() {
                         <h3>Projects</h3>
                         <p>As a student, I tracked my journey in development producting some projects</p>
                     </div>
-                    <Link href="#">View More <ArrowIcon /></Link>
+                    <Link href="#">View More <ArrowIcon/></Link>
                 </div>
-                <ProjectsCard project={"luna"} />
-                <ProjectsCard project={"spring-project"} />
-                <ProjectsCard project={"rr-html"} />
+                <div className={s.cardWrapper}>
+                    <ProjectsCard project={"luna"}/>
+                    <ProjectsCard project={"spring-project"}/>
+                </div>
             </section>
         </div>
     )
