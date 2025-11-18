7cb30734e2bf5da95ac672a37c4a3138ea015905
lyanbrito <lyanbrito613@gmail.com>
2025-11-17 21:14:57 -0300

    assets: deleated unused icons

--- apps/portifolio-content/src/assets/icons/CSSIcon.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-
-const CSSIcon = (props: SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="84" height="77" viewBox="0 0 84 77" fill="none">
-        <path d="M12.495 0H84L71.365 64.148L33.166 77L0 64.148L3.374 47.019H17.5L16.1 54.096L36.148 61.845L59.248 54.096L62.475 37.8H5.075L7.84 23.485H65.282L67.088 14.294H9.688L12.495 0Z" fill="#0C8ADD" />
-    </svg>
-);
-export default CSSIcon;
