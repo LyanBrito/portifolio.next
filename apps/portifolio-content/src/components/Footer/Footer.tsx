efc09de64e02808c6ce90ec0186c4e72fcfb6651
lyanbrito <lyanbrito613@gmail.com>
2025-11-17 22:52:51 -0300

    feat: cerated footer component

---
+++ apps/portifolio-content/src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import s from "./Footer.module.scss";
+import Link from "next/link";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
+
+type FooterProps = {
+    page: string,
+}
+
+export default function Footer({page}: FooterProps) {
+    return (
+        <footer className={s.footerContainer}>
+            <p>2025 © — Lyan Brito</p>
+            <nav className={s[page]}>
+                <Link href="#home">Home</Link>
+                <Link href="#about-me">About Me</Link>
+                <Link href="#projects">Projects</Link>
+                <Link href="#home">Go Up<ArrowIcon /></Link>
+            </nav>
+        </footer>
+    )
+}
