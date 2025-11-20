7b0b4b9ecefdd46c08a889743c6c36e6d4138c4d
lyanbrito <lyanbrito613@gmail.com>
2025-11-20 18:58:03 -0300

    wip: creating about-me page

--- apps/portifolio-content/src/pages/AboutMePage/AboutMe.tsx
+++ apps/portifolio-content/src/pages/AboutMePage/AboutMe.tsx
@@ -1,10 +1,27 @@
 import NavBar from "@/components/NavBar/NavBar";
+import s from "./AboutMe.module.scss";
+import hs from "../HomePage/HomePage.module.scss"
+import DownloadIcon from "@/assets/icons/DownloadIcon";
+import Link from "next/link";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
+import SectionLink from "@/components/SectionLink/SectionLink";
 
 export default function AboutMePage() {
     return (
-        <>
+        <div className={hs.homeContainer}>
             <NavBar page={"about-me"} links={["/", "/AboutMePage/AboutMe", "#projects", "contacts"]}/>
-            <h1>oi</h1>
-        </>
+            <section className={s.titleWrapper}>
+                <h1>About me</h1>
+                <p>As a student, I tracked my journey in development producting some projectsAs a student, I tracked my
+                    journey in development producting some projects</p>
+                <div className={s.downloadResume}>
+                    <span>Resume</span>
+                    <DownloadIcon/>
+                </div>
+            </section>
+            <section className={s.stacksWrapper}>
+               <SectionLink isBlank={true} title="About Me" description="These are my best skills sei la texto grandinho aqui" linkLabel="View in github" linkRef="https://github.com/LyanBrito" />
+            </section>
+        </div>
     )
 }
