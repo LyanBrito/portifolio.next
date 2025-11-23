bf2448d8000daaa14cda4553a795c78f1fedf8d3
lyanbrito <lyanbrito613@gmail.com>
2025-11-23 18:06:02 -0300

    assets: added resume download button

--- apps/portifolio-content/src/pages/AboutMePage/AboutMe.tsx
+++ apps/portifolio-content/src/pages/AboutMePage/AboutMe.tsx
@@ -1,60 +1,66 @@
 import NavBar from "@/components/NavBar/NavBar";
 import s from "./AboutMe.module.scss";
-import hs from "../HomePage/HomePage.module.scss"
-import DownloadIcon from "@/assets/icons/DownloadIcon";
-import Link from "next/link";
-import ArrowIcon from "@/assets/icons/ArrowIcon";
+import hs from "../HomePage/HomePage.module.scss";
 import SectionLink from "@/components/SectionLink/SectionLink";
 import SkillsWrapper from "@/components/SkillsWrapper/SkilsWrapper";
+import Footer from "@/components/Footer/Footer";
+import DownloadIcon from "@/assets/icons/DownloadIcon";
+import Link from "next/link";
 
 export default function AboutMePage() {
     return (
         <div className={hs.homeContainer}>
-            <NavBar page={"about-me"} links={["/", "/AboutMePage/AboutMe", "#projects", "contacts"]} />
-            <section className={s.titleWrapper}>
-                <h1>About me</h1>
-                <p>As a student, I tracked my journey in development producting some projectsAs a student, I tracked my
-                    journey in development producting some projects</p>
-                <div className={s.downloadResume}>
-                    <span>Resume</span>
-                    <DownloadIcon />
+            <NavBar page={"about-me"} links={["/", "/AboutMePage/AboutMe", "#projects", "contacts"]}/>
+            <section className={s.aboutContainer}>
+                <div className={s.titleWrapper}>
+                    <h1>About me</h1>
+                    <a href="../../assets/docs/Resume.pdf" download className={`${hs.ATWcontainer} ${s.dlResume}`}>
+                        <DownloadIcon/>
+                        <span>Resume</span>
+                    </a>
                 </div>
+                <p>I'm a development student passionate about technology. I track my growth through real projects,
+                    always aiming to learn, create, and improve.
+                </p>
             </section>
             <section className={s.stacksWrapper}>
-                <SectionLink isBlank={true} title="About Me" description="These are my best skills sei la texto grandinho aqui" linkLabel="View in github" linkRef="https://github.com/LyanBrito" />
+                <SectionLink isBlank={true} title="My Stacks"
+                             description="These are the technologies I use the most in my projects and represent my current skill set as a developer."
+                             linkLabel="View in github" linkRef="https://github.com/LyanBrito"/>
             </section>
             <SkillsWrapper
                 title="Frontend"
                 isBlank={true}
-                linkLabel="oi"
-                linkRef="#"
-                skills={["React", "NextJS", "TypeScript", "Turborepo"]}
+                linkLabel=""
+                linkRef=""
+                skills={["HTML", "CSS", "SCSS", "Bootstrap", "TailwindCSS", "styled-components", "less.JS", "React", "NextJS"]}
                 description=""
             />
             <SkillsWrapper
                 title="Backend"
                 isBlank={true}
-                linkLabel="oi"
-                linkRef="#"
-                skills={["React", "Java"]}
+                linkLabel=""
+                linkRef=""
+                skills={["Java", "Spring"]}
                 description=""
             />
             <SkillsWrapper
                 title="Tools"
                 isBlank={true}
-                linkLabel="oi"
-                linkRef="#"
-                skills={["React", "Java"]}
+                linkLabel=""
+                linkRef=""
+                skills={["Postman", "Figma", "Git", "Vercel", "Github", "Biome"]}
                 description=""
             />
             <SkillsWrapper
                 title="Studiyng"
                 isBlank={true}
-                linkLabel="oi"
-                linkRef="#"
-                skills={["React", "Java"]}
+                linkLabel=""
+                linkRef=""
+                skills={["NodeJS", "Maven", "ReactNative", "module-federation", "Turborepo", "MySQL"]}
                 description=""
             />
+            <Footer page="other"/>
         </div>
     )
 }
