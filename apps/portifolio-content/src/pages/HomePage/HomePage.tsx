87befd878a2032946fbd714477037f40b1aa65a1
lyanbrito <lyanbrito613@gmail.com>
2025-11-25 15:35:33 -0300

    feat: added contacts-page link

--- apps/portifolio-content/src/pages/HomePage/HomePage.tsx
+++ apps/portifolio-content/src/pages/HomePage/HomePage.tsx
@@ -9,6 +9,7 @@ import WhatsappIcon from "@/assets/icons/WhatsappIcon";
 import MailIcon from "@/assets/icons/MailIcon";
 import Footer from "@/components/Footer/Footer";
 import SectionLink from "@/components/SectionLink/SectionLink";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
 
 export default function HomePage() {
     return (
@@ -52,6 +53,10 @@ export default function HomePage() {
                 <div className={s.socialWrapepr}>
                     <Link className={s.socialLink} href="mailto:lyanbrito613@gmail.com"><MailIcon />lyanbrito613@gmail.com</Link>
                     <Link className={s.socialLink} href="tel:+5511986581730"><WhatsappIcon />(11) 9 8658-1730</Link>
+                    <Link className={s.button}
+                        href="/ContactsPage/Contacts">
+                        More Contact Info <ArrowIcon />
+                    </Link>
                 </div>
             </section>
             <Footer page="home" />
