a971f6fc5117e3266975735624dd2af78ac3e69d
lyanbrito <lyanbrito613@gmail.com>
2026-01-29 11:15:41 -0300

    feat: added dependencies & config mf

--- apps/portifolio-content/next.config.js
+++ apps/portifolio-content/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+
+// const remotes = isServer => {
+//     const location = isServer ? 'ssr' : 'chunks';
+//     return {
+//         shell: `shell@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+//     };
+// };
+
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
@@ -10,4 +22,26 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+    webpack(config, options) {
+        config.plugins.push(
+            new NextFederationPlugin({
+                name: 'portifolio-content',
+                filename: 'static/chunks/remoteEntry.js',
+                dts: false,
+                exposes: {
+                    './home': './src/pages/index.tsx',
+                },
+                // remotes: remotes(options.isServer),
+                shared: {
+                    // reactStrictMode: true,
+                },
+                extraOptions: {
+                    exposePages: true,
+                },
+            }),
+        );
+
+        return config;
+    },
+};
