0c77a9874c8c3df889d9c080f1ad67e7759badf0
lyanbrito <lyanbrito613@gmail.com>
2026-01-30 14:45:35 -0300

    fix: added website tittle in a propper way

--- apps/portifolio-content/src/pages/_app.tsx
+++ apps/portifolio-content/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from 'next/head'
 import "../styles/main.scss"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>LyanBrito`s Portfolio</title>
+            </Head>
+
+            <Component {...pageProps} />
+        </>)
 }
