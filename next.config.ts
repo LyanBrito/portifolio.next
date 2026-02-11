13eaa3eb58ce6b7083b53cd54aad29560e088217
lyanbrito <lyanbrito613@gmail.com>
2026-02-11 16:47:00 -0300

    chore: added browser config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@ import type {NextConfig} from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+    productionBrowserSourceMaps: true,
     /* config options here */
     reactCompiler: true,
-        sassOptions: {
+    sassOptions: {
         includePaths: [path.join(__dirname, "src")],
+
+        //         sassOptions: {
+        //   includePaths: [path.join(__dirname, "src")],
+        //   additionalData: `@use "styles/utils" as *;`
+        // }
     },
 };
 
