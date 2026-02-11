7fd18abbd9ffa5ea822aa82bf6e4ac06f006999d
lyanbrito <lyanbrito613@gmail.com>
2026-02-11 16:46:24 -0300

    feat: destructured page & separate client side

---
+++ src/app/HomePage/HomeClient.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {useEffect, useState} from "react";
+import HomeContent from "@/app/HomePage/HomeContent";
+
+export default function HomeClient() {
+
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 800)
+        return () => clearTimeout(timer)
+    }, [])
+
+    return <HomeContent/>
+}
