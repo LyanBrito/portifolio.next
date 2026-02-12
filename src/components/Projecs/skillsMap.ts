ba13973060b9e4c9604602d2c3444298fe529141
lyanbrito <lyanbrito613@gmail.com>
2026-02-12 11:10:45 -0300

    chore: throw out dev-icons/react

--- src/components/Projecs/skillsMap.ts
+++ src/components/Projecs/skillsMap.ts
@@ -1,45 +1,53 @@
 import React from "react";
 
-import {
-    AzuresqldatabaseOriginal, BiomeOriginal, BootstrapOriginal,
-    Css3Original,
-    FigmaOriginal, GithubOriginal,
-    GitOriginal, Html5Original,
-    JavaOriginal, JavascriptOriginal,
-    LessPlainWordmark, MavenOriginal, MysqlOriginal, NextjsOriginal, NodejsOriginal, PostmanOriginal,
-    ReactnativeOriginal,
-    ReactOriginal, SassOriginal,
-    SpringOriginal, StyledcomponentsOriginal, TailwindcssOriginal,
-    TypescriptOriginal, VercelOriginal, ViteOriginal, WebpackOriginal
-} from "devicons-react";
-import TurborepoIcon from "@/assets/icons/TurborepoIcon";
+import TurborepoIcon from "@/assets/icons/dev-icons/TurborepoIcon";
+import ReactIcon from "@/assets/icons/dev-icons/ReactIcon";
+import SpringIcon from "@/assets/icons/dev-icons/SpringIcon";
+import GitIcon from "@/assets/icons/dev-icons/GitIcon";
+import FigmaIcon from "@/assets/icons/dev-icons/FigmaIcon";
+import TsIcon from "@/assets/icons/dev-icons/TsIcon";
+import JsIcon from "@/assets/icons/dev-icons/JsIcon";
+import HtmlIcon from "@/assets/icons/dev-icons/HtmlIcon";
+import CssIcon from "@/assets/icons/dev-icons/CssIcon";
+import ScssIcon from "@/assets/icons/dev-icons/ScssIcon";
+import JavaIcon from "@/assets/icons/dev-icons/JavaIcon";
+import MySQLIcon from "@/assets/icons/dev-icons/MySQLIcon";
+import TailwindIcon from "@/assets/icons/dev-icons/TailwindIcon";
+import StyledComponentsIcon from "@/assets/icons/dev-icons/StyledComponentsIcon";
+import BootstrapIcon from "@/assets/icons/dev-icons/BootstrapIcon";
+import PostmanIcon from "@/assets/icons/dev-icons/PostmanIcon";
+import VercelIcon from "@/assets/icons/dev-icons/VercelIcon";
+import BiomeIcon from "@/assets/icons/dev-icons/BiomeIcon";
+import NodeJSIcon from "@/assets/icons/dev-icons/NodeJSIcon";
+import MavenIcon from "@/assets/icons/dev-icons/MavenIcon";
+import MFEIcon from "@/assets/icons/dev-icons/MFEIcon";
+import GithubIcon from "@/assets/icons/dev-icons/GithubIcon";
 
 export const skillsIcons: Record<string, React.FC> = {
-    React: ReactOriginal,
-    Spring: SpringOriginal,
-    Git: GitOriginal,
-    Figma: FigmaOriginal,
-    TypeScript: TypescriptOriginal,
-    JavaScript: JavascriptOriginal,
-    HTML: Html5Original,
-    CSS: Css3Original,
-    SCSS: SassOriginal,
-    NextJS: NextjsOriginal,
-    Java: JavaOriginal,
-    MySQL: MysqlOriginal,
-    TailwindCSS: TailwindcssOriginal,
-    "styled-components" : StyledcomponentsOriginal,
-    Bootstrap: BootstrapOriginal,
-    Vite: ViteOriginal,
-    Postman: PostmanOriginal,
-    Vercel: VercelOriginal,
-    BiomeJS: BiomeOriginal,
-    NodeJS: NodejsOriginal,
-    Maven: MavenOriginal,
-    ReactNative: ReactnativeOriginal,
-    "module-federation": WebpackOriginal,
+    React: ReactIcon,
+    Spring: SpringIcon,
+    Git: GitIcon,
+    Figma: FigmaIcon,
+    TypeScript: TsIcon,
+    JavaScript: JsIcon,
+    HTML: HtmlIcon,
+    CSS: CssIcon,
+    SCSS: ScssIcon,
+    // NextJS: NextJs,
+    Java: JavaIcon,
+    MySQL: MySQLIcon,
+    TailwindCSS: TailwindIcon,
+    "styled-components": StyledComponentsIcon,
+    Bootstrap: BootstrapIcon,
+    // Vite: ViteOriginal,
+    Postman: PostmanIcon,
+    Vercel: VercelIcon,
+    BiomeJS: BiomeIcon,
+    NodeJS: NodeJSIcon,
+    Maven: MavenIcon,
+    // ReactNative: ReactnativeOriginal,
+    "module-federation": MFEIcon,
     Turborepo: TurborepoIcon,
-    lessJS: LessPlainWordmark,
-    Github: GithubOriginal,
-    Biome: BiomeOriginal,
+    // lessJS: LessPlainWordmark,
+    Github: GithubIcon,
 };
