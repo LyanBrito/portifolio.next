202fc185cb60bb34602a9cc5d3c59173fc75939d
lyanbrito <lyanbrito613@gmail.com>
2026-02-12 11:02:39 -0300

    assets: added static icons

---
+++ src/assets/icons/dev-icons/VercelIcon.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import type {SVGProps} from "react";
+
+const VercelIcon = (props: SVGProps<SVGSVGElement>) => (
+
+    <svg viewBox="0 0 128 128">
+        <path d="M64.002 8.576 128 119.424H0Zm0 0"></path>
+    </svg>
+
+);
+export default VercelIcon;
