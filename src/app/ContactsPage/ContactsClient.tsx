2dc37f0b8574b8e5c1a3d3adb0aa0203a6ae25fb
lyanbrito <lyanbrito613@gmail.com>
2026-02-13 16:03:41 -0300

    refactor: destructured contactsPage client side

---
+++ src/app/ContactsPage/ContactsClient.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import {useEffect, useRef, useState} from "react";
+import ContactsSK from "@/components/ui/skeleton/ContactsSK";
+import ContactsContent from "@/app/ContactsPage/ContactsContent";
+
+export default function ContactsClient() {
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 800)
+        return () => clearTimeout(timer)
+    }, [])
+
+    if (loading) return <ContactsSK />
+
+    return (
+        <ContactsContent />
+    )
+}
